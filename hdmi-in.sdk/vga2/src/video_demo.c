d2a3d36bd3473e44753a2fba7ef0b36c877e22e8
jswinther <35611281+jswinther@users.noreply.github.com>
2019-01-14 18:45:44 +0100

    das

--- hdmi-in.sdk/vga2/src/video_demo.c
+++ hdmi-in.sdk/vga2/src/video_demo.c
@@ -50,6 +50,7 @@
 #include "numberArray.h"
 #include "score.h"
 #include "Framebuffer.h"
+#include "background.h"
 /* ------------------------------------------------------------ */
 /*						   Defines				        		*/
 /* ------------------------------------------------------------ */
@@ -139,20 +140,19 @@ void DemoStartGame() {
 
 		while (btn_value == 1) {
 			while(dead != 1) {
-				/*
-
-				*/
 				Overwrite(frameBuf[frame]);
 				Move(frameBuf[frame]);
 				Print(frameBuf[frame]);
-
 				Xil_DCacheFlushRange((unsigned int)frameBuf[frame], DEMO_MAX_FRAME);
 				DisplayChangeFrame(&dispCtrl, frame);
-				DemoPrintBackground(frameBuf[frame]);
-				frame++;
+				//Overwrite(frameBuf[frame]);
+				/*
+				 frame++;
 				if (frame >= DISPLAY_NUM_FRAMES) {
 					frame = 0;
 				}
+				 */
+
 
 			}
 			resetf = 1;
@@ -273,7 +273,7 @@ void Print(u8 *frame) {
 }
 
 void DemoPrintBackground(u8 *frame) {
-	for(int i = 0; i < 1080; i++) {
+	for(int i = 0; i < 1080*1920*3; i++) {
 		frame[i] = 255;
 	}
 }
