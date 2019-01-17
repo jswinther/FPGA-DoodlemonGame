cebcdd44cfddca1e2e2d11e1708279220a5c929c
jswinther <35611281+jswinther@users.noreply.github.com>
2019-01-17 20:16:26 +0100

    Ny Projekt og BSP samt Hardware Wrapper

---
+++ hdmi-in.sdk/Doodlemon/src/Score.h
@@ -0,0 +1,120 @@
+/*
+ * Score.h
+ *
+ *  Created on: 14. jan. 2019
+ *      Author: Nicolai
+ */
+#include "game.h"
+
+#ifndef SRC_SCORE_H_
+#define SRC_SCORE_H_
+
+u32 platformspeed = 15;
+
+
+
+
+u8 speedCounter = 0;
+
+
+
+u16 incrementInterval = 5;
+u8 intervalCount = 0;
+
+
+
+u16 currentScoreCounter = 0;
+u16 highscoreCounter = 0;
+u16 averageScoreCounter = 0;
+
+
+int ones = 0;
+int tens = 0;
+int hundreds = 0;
+int thousands = 0;
+
+int highones = 0;
+int hightens = 0;
+int highhundreds = 0;
+int highthousands = 0;
+
+u8 avgones = 0;
+u8 avgtens = 0;
+u8 avghundreds = 0;
+u8 avgthousands = 0;
+
+//Average score
+u8 averageScore = 0;
+u8 totalScore = 0;
+
+
+//Times played
+u8 gamesPlayed = 1;
+
+
+void findAverageScore(){
+	totalScore += (ones+(10*tens)+(100*hundreds)+(1000*thousands));
+	averageScore = totalScore/gamesPlayed;
+	xil_printf("Average score: %d", averageScore);
+}
+void Increment() {
+	ones++;
+	currentScoreCounter++;
+	intervalCount++;
+	if(intervalCount == incrementInterval) {
+		intervalCount = 0;
+		incrementInterval++;
+		platformspeed+=3;
+	}
+
+	if(ones == 10) {
+		tens++;
+		ones = 0;
+	}
+	if(tens == 10) {
+		hundreds++;
+		tens = 0;
+
+	}
+	if(hundreds == 10) {
+		thousands++;
+		hundreds = 0;
+	}
+}
+
+void resetScore(){
+	if(currentScoreCounter > highscoreCounter) {
+		highscoreCounter = currentScoreCounter;
+		highones = ones;
+		hightens = tens;
+		highhundreds = hundreds;
+		highthousands = thousands;
+		gamesPlayed++;
+		currentScoreCounter = 0;
+		findAverageScore();
+		char cones = (char) ones;
+		char ctens = (char) tens;
+		char chundreds = (char) hundreds;
+		char cthousands = (char) thousands;
+		char num[] = {cones, ctens, chundreds, cthousands};
+		ones = 0;
+		tens = 0;
+		hundreds = 0;
+		thousands = 0;
+		SDWrite(num, 4);
+
+	} else {
+		gamesPlayed++;
+		currentScoreCounter = 0;
+		findAverageScore();
+		ones = 0;
+		tens = 0;
+		hundreds = 0;
+		thousands = 0;
+	}
+	intervalCount = 0;
+	incrementInterval = 5;
+
+}
+
+#endif /* SRC_SCORE_H_ */
