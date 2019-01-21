dcb165053bf7dacd930ace0098baac2bde9878cb
Nicolai <nbr2601@gmail.com>
2019-01-21 15:19:37 +0100

    Fyr den auf!!

---
+++ hdmi-in.sdk/DoodlemonV2/Debug/src/SDcard/platform.d
@@ -0,0 +1,16 @@
+src/SDcard/platform.o src/SDcard/platform.o: ../src/SDcard/platform.c \
+ ../../DoodlemonV2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../DoodlemonV2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../DoodlemonV2_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../DoodlemonV2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/SDcard/platform_config.h
+
+../../DoodlemonV2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../DoodlemonV2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../DoodlemonV2_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../DoodlemonV2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/SDcard/platform_config.h:
